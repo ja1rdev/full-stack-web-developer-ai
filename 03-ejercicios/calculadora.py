f48d94893d7c8844ed11317417db7e1c93da5ee6
ja1rdev <jairenriquez1715@gmail.com>
2025-09-26 22:53:11 -0500

    calculator added

--- 03-ejercicios/calculadora.py
+++ 03-ejercicios/calculadora.py
@@ -1,48 +1,37 @@
-# Calculadora que permita realizar operaciones aritméticas básicas.
-def sumar(a, b):
+# Definir la funciones
+def suma (a, b):
     return a + b
 
-def restar(a, b):
+def resta (a, b):
     return a - b
 
-def multiplicar(a, b):
+def multiplicacion (a, b):
     return a * b
 
-def dividir(a, b):
-    return a / b if b != 0 else "Error: División entre 0"
-
-while True:
-    print("\nCalculadora aritmética...")
-    print("1. Sumar")
-    print("2. Restar")
-    print("3. Multiplicar")
-    print("4. Dividir")
-    print("5. Salir")
-
-    try:
-        opcion = int(input("Selecciona una opción: "))
-    except ValueError:
-        print("Por favor. Ingresa un número del 1 a 5.")
-        continue
-
-    if 1 <= opcion <= 4:
-        try:
-            a = int(input("Primer número: "))
-            b = int(input("Segundo número: "))
-        except ValueError:
-            print("Debes ingresar números enteros.")
-            continue
-        
-        if opcion == 1:
-            print("Suma =",sumar(a, b))
-        elif opcion == 2:
-            print("Resta =", restar(a, b))
-        elif opcion == 3:
-            print("Multiplicación =", multiplicar(a, b))
-        elif opcion == 4:
-            print("División =", dividir(a, b))
-    elif opcion == 5:
-        print("Saliendo de la calculadora...")
-        break
+def division (a, b):
+    if b == 0:
+        return "Error: No se puede dividir por cero"
     else:
-        print("Opción no válida. Inténtalo de nuevo.")
+        a / b
+
+# Obtener la entradas
+try:
+    num1 = float(input("Ingresa el primero número: "))
+    operador = input("Ingresa la operación (+, -, *, /): ")
+    num2 = float(input("Ingresa el segundo número: "))
+
+    if operador == '+':
+        resultado = suma(num1, num2)
+    elif operador == '-':
+        resultado = resta(num1, num2)
+    elif operador == '*':
+        resultado = multiplicacion(num1, num2)
+    elif operador == '/':
+        resultado = division(num1, num2)
+    else:
+        resultado = "Operación no válida"
+    
+    print(f"El resultado es : {resultado}")
+
+except ValueError:
+   print("Entrada no válida. Por favor, ingresa números")
