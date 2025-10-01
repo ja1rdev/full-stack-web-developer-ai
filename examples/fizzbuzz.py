a6a4eae2e9963b2e9eac23debe7cfe227cce2c1f
ja1rdev <jairenriquez1715@gmail.com>
2025-10-01 09:30:57 -0500

    fizzbuzz added

--- examples/fizzbuzz.py
+++ examples/fizzbuzz.py
@@ -1,9 +1,9 @@
 for i in range(1, 101):
    if i % 5 == 0 and i % 3 == 0:
-      print("fizzbuzz")
+      print("FizzBuzz")
    elif i % 3 == 0:
-      print("fizz")
+      print("Fizz")
    elif i % 5 == 0:
-      print("buzz")
+      print("Buzz")
    else:
       print(i)
