aee3a2185d130aec9f4c074cb5c7221d9630abbb
ja1rdev <jairenriquez1715@gmail.com>
2025-10-20 19:08:48 -0500

    challege added

---
+++ challenges/challege.py
@@ -0,0 +1,9 @@
+# Triangle area calculator
+
+base = float(input("Enter the base of the triangle: "))
+
+height = float(input("Entr the height of the triangle: "))
+
+area = (base * height) / 2
+
+print("Area of the triangle: ", area)
