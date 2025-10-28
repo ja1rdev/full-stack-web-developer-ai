042cf897cd33407afc2d872bd5da15cd72326aba
ja1rdev <jairenriquez1715@gmail.com>
2025-10-28 10:42:54 -0500

    conditionals2 added

---
+++ 03-ejercicios/conditionals2.py
@@ -0,0 +1,6 @@
+number = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+number3 = int(input("Enter the third number: "))
+
+largest = max(number, number2, number3)
+print(f"The largest number is {largest}")
